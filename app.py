9f8d6f71ff2f9b33f0eba87b11ac94470afe8624
Kenneth Albert <gichuka@gmail.com>
2025-10-29 16:53:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -48,12 +48,13 @@ if st.button('Diabetes Test Result'):
     prediction_result = diabetes_prediction(input_data)
 
     if prediction_result == 'The person is not diabetic':
-        st.success(prediction_result)
-        st.balloons()
-
-    else:
         st.error(prediction_result)
         st.info("Please consult with a healthcare professional for proper diagnosis and treatment.")
+        
+    else:
+        st.success(prediction_result)
+        st.balloons()
+        
 with st.expander("📊 About this Model"):
     st.write("""
         **Model Performance:**
